997dbcd35a30569cd25e8bea542985b2c99fa64e
jcmartinez <jcmartinezcu@gmail.com>
2024-08-15 16:38:17 +0200

    implementando rest controller

--- src/main/java/com/jc/curso/springboot/app/interceptor/springbootinterceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/jc/curso/springboot/app/interceptor/springbootinterceptor/interceptors/LoadingTimeInterceptor.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,17 +17,19 @@ public class LoadingTimeInterceptor implements HandlerInterceptor{
     private static final Logger Logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
     
 
-    @SuppressWarnings("null")
+
     @Override
     public void postHandle( HttpServletRequest request,  HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
-        Logger.info("LoadingTimerInterceptor: postHandler() saliendo ....");
+        Logger.info("LoadingTimerInterceptor: postHandler() saliendo .... " + ((HandlerMethod) handler).getMethod().getName());
     }
 
-    @SuppressWarnings("null")
+    
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
-        Logger.info("LoadingTimerInterceptor: preHandler() entrando ....");
+
+        HandlerMethod controller =((HandlerMethod) handler);
+        Logger.info("LoadingTimerInterceptor: preHandler() entrando .... "  + controller.getMethod().getName());
         return true;
     }
 
