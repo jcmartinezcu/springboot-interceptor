d31dd034e6c75eaf64b1de7388c622a3a218c042
jcmartinez <jcmartinezcu@gmail.com>
2024-08-15 17:00:43 +0200

    interceptor incluir o excluir rutas

--- src/main/java/com/jc/curso/springboot/app/interceptor/springbootinterceptor/MvcConfig.java
+++ src/main/java/com/jc/curso/springboot/app/interceptor/springbootinterceptor/MvcConfig.java
@@ -18,7 +18,8 @@ public class MvcConfig implements WebMvcConfigurer
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
      
-       registry.addInterceptor(timeInterceptor);
+       registry.addInterceptor(timeInterceptor).addPathPatterns("/app/bar", "/app/foo");
+       //registry.addInterceptor(timeInterceptor).excludePathPatterns("/app/bar");
     }
 
     
