f0043e02fff0869abeb191fa8270fa8960d1cc58
jcmartinez <jcmartinezcu@gmail.com>
2024-08-15 16:25:17 +0200

    registrando interceptor en la configuracion

--- src/main/java/com/jc/curso/springboot/app/interceptor/springbootinterceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/jc/curso/springboot/app/interceptor/springbootinterceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.jc.curso.springboot.app.interceptor.springbootinterceptor.interceptors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -8,20 +10,23 @@ import org.springframework.web.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+@Component("timeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor{  
+
+    private static final Logger Logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
     
 
     @SuppressWarnings("null")
     @Override
     public void postHandle( HttpServletRequest request,  HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
-        
+        Logger.info("LoadingTimerInterceptor: postHandler() saliendo ....");
     }
 
     @SuppressWarnings("null")
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+        Logger.info("LoadingTimerInterceptor: preHandler() entrando ....");
         return true;
     }
 
