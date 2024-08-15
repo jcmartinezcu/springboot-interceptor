c14e1afa88961b771b3ac73c26a93f3ad3779b5f
jcmartinez <jcmartinezcu@gmail.com>
2024-08-15 16:49:53 +0200

    implementando interceptor tiempo transcurrido

--- src/main/java/com/jc/curso/springboot/app/interceptor/springbootinterceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/jc/curso/springboot/app/interceptor/springbootinterceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.jc.curso.springboot.app.interceptor.springbootinterceptor.interceptors;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
@@ -14,25 +16,37 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component("timeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor{  
 
-    private static final Logger Logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
+    private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
     
-
-
-    @Override
-    public void postHandle( HttpServletRequest request,  HttpServletResponse response, Object handler,
-            @Nullable ModelAndView modelAndView) throws Exception {
-        Logger.info("LoadingTimerInterceptor: postHandler() saliendo .... " + ((HandlerMethod) handler).getMethod().getName());
-    }
-
     
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
 
         HandlerMethod controller =((HandlerMethod) handler);
-        Logger.info("LoadingTimerInterceptor: preHandler() entrando .... "  + controller.getMethod().getName());
+        logger.info("LoadingTimerInterceptor: preHandler() entrando .... "  + controller.getMethod().getName());
+        
+        long start = System.currentTimeMillis();
+        request.setAttribute("start", start);
+        Random random = new Random();
+        int delay = random.nextInt(500);
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+           
+            e.printStackTrace();
+        }
         return true;
     }
 
-    
+    @Override
+    public void postHandle( HttpServletRequest request,  HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+        
+         long end = System.currentTimeMillis();
+         long start = (long) request.getAttribute("start");
+         long result = end - start;
+         logger.info("Tiempo transcurrido: " + result + " milisegundos!");
+         logger.info("LoadingTimerInterceptor: postHandler() saliendo .... " + ((HandlerMethod) handler).getMethod().getName());
+    }
 
 }
